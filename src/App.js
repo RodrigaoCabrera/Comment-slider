59bb780cec4bc53db80f68ac1bb721d798760198
Rodrigo Cabrera <rodrigod33d@gmail.com>
2022-06-18 14:55:33 -0300

    ADD: Estructura inicial + estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+//import de imágenes
+import profile from "./assets/profile.png";
+import ro from "./assets/ro.png";
+import next from "./assets/next.svg";
+import prev from "./assets/prev.svg";
+
+//Import de styled component
+import styled from "styled-components";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SliderContainer>
+      <SliderChildren>
+        <Button>
+          <img src={prev} />
+        </Button>
+
+        <CardContainer>
+          <Card>
+            <Profile>
+              <ProfileImg src={ro} alt="foto de perfil" />
+              <h1>Jhon Doe</h1>
+            </Profile>
+            <CommentContainer>
+              <p>
+                Me pareció excelente, creo que el Discord está muy bien
+                organizado y la iniciativa de NUWE es tremenda, gracias por la
+                oportunidad, realmente lo disfruté.
+              </p>
+            </CommentContainer>
+          </Card>
+          <Card>
+            <Profile>
+              <img src={profile} alt="foto de perfil" />
+              <h1>Jhon Doe</h1>
+            </Profile>
+            <CommentContainer>
+              <p>
+                Me pareció excelente, creo que el Discord está muy bien
+                organizado y la iniciativa de NUWE es tremenda, gracias por la
+                oportunidad, realmente lo disfruté.
+              </p>
+            </CommentContainer>
+          </Card>
+        </CardContainer>
+
+        <Button>
+          <img src={next} />
+        </Button>
+      </SliderChildren>
+    </SliderContainer>
   );
 }
+const SliderContainer = styled.div`
+  background-color: #252835;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
 
-export default App;
+const SliderChildren = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+const CardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  max-width: 508px;
+`;
+const Card = styled.section`
+  min-width: 100%;
+  height: 258.58px;
+  background: #252835;
+  border: 2px solid #2e353f;
+  box-shadow: 10px 10px 50px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
+
+const Button = styled.button`
+  width: 34px;
+  height: 36px;
+  background: #1e212e;
+  border: 1px solid #39424e;
+  border-radius: 5px;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+`;
+
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 1rem;
+`;
+
+const CommentContainer = styled.div`
+  padding: 1rem;
+`;
+
+const ProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+`;
